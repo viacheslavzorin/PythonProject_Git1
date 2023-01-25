4159581163dcd2033ebbe728af767ec79c657f00
viacheslavzorin <zorin-viacheslav@rambler.ru>
2023-01-25 18:34:00 +0300

    Добавление функции вычитания

--- funcs.py
+++ funcs.py
@@ -2,3 +2,8 @@ def get_addition():
     a=int(input("Введите число "))
     b=int(input("Введите число "))
     print(a+b)
+
+def get_subtraction():
+    a = int(input("Введите число "))
+    b = int(input("Введите число "))
+    print(a-b)
